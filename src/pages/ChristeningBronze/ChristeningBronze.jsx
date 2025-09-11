d130341f32c804c48b4616019a8f69ca3771b15c
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-11 16:27:57 +0200

    Update ChristeningBronze and data

--- src/pages/ChristeningBronze/ChristeningBronze.jsx
+++ src/pages/ChristeningBronze/ChristeningBronze.jsx
@@ -3,26 +3,34 @@ import confetti from "canvas-confetti";
 import img from "/christening/02.webp";
 import img1 from "/christening/04.webp";
 import img2 from "/christening/06.webp";
-import img3 from "/christening/01.webp";
+import img4 from "/christening/07.webp";
+import img5 from "/christening/05.webp";
+import img6 from "/christening/01.webp";
 import baloonIcon from "/icons/balloon.png";
 import data from "./../../assets/data/data.json";
-
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-cards";
-
-// import required modules
-import { EffectCards } from "swiper/modules";
-
+import playIcon from "/icons/mute.png";
+import pauseIcon from "/icons/unmute.png";
 import { useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import "./ChristeningBronze.scss";
 
 const ChristeningBronze = () => {
 	const { id } = useParams();
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	const handlePlayAudio = () => {
+		setIsPlaying((prev) => !prev);
+	};
+
+	useEffect(() => {
+		const audio = document.querySelector(".audio");
+
+		if (!isPlaying) {
+			audio.pause();
+		} else {
+			audio.play();
+		}
+	}, [isPlaying]);
 
 	const envelope = data.find((envelope) => envelope.id == id);
 
@@ -174,6 +182,18 @@ const ChristeningBronze = () => {
 	}, []);
 
 	useEffect(() => {
+		const animatedElements = document.querySelectorAll(".animated-element1");
+
+		window.addEventListener("scroll", () => {
+			animatedElements.forEach((el) => {
+				const elRect = el.getBoundingClientRect();
+
+				if (elRect.top < window.innerHeight - 100) {
+					el.classList.add("animated-element1--active");
+				}
+			});
+		});
+
 		const rainbow = document.querySelectorAll(".rainbow");
 
 		rainbow.forEach((el) => {
@@ -189,30 +209,64 @@ const ChristeningBronze = () => {
 					});
 				}
 			});
-
-			const elRect = el.getBoundingClientRect();
-			if (elRect.top < window.innerHeight) {
-				rainbows.forEach((el, index) => {
-					setTimeout(() => {
-						el.classList.add("rainbow-el--active");
-					}, 100 * index);
-				});
-			}
 		});
 	}, []);
+
+	useEffect(() => {
+		document.body.style.overflow = "hidden";
+	}, []);
+
+	const handleLoading = () => {
+		document.body.style.overflow = "auto";
+		document.querySelector(".loading-chr").classList.add("loading--hide");
+		document.querySelector(".home-chr").style.display = "flex";
+		setIsPlaying(true);
+		setTimeout(() => {
+			document
+				.querySelector(".home__top-chr")
+				.classList.add("home__top-chr--active");
+
+			const rainbow = document.querySelectorAll(".rainbow");
+
+			rainbow.forEach((el) => {
+				const rainbows = el.querySelectorAll(".pp");
+
+				const elRect = el.getBoundingClientRect();
+				if (elRect.top < window.innerHeight) {
+					rainbows.forEach((el, index) => {
+						setTimeout(() => {
+							el.classList.add("rainbow-el--active");
+						}, 100 * index);
+					});
+				}
+			});
+		}, 1000);
+	};
 
 	return (
 		<>
 			{showHeader && <Header />}
-			<main className="home-chr">
+			<div onClick={handleLoading} className="loading-chr">
+				<img src={img1} alt="" />
+				<img src={img1} alt="" />
+				<img src={img1} alt="" />
+				<img src={img1} alt="" />
+				<img src={img1} alt="" />
+				<img src={img1} alt="" />
+				<img src={img1} alt="" />
+				<div className="loading-txt">
+					Торкніться екрана, щоб відкрити запрошення!
+				</div>
+			</div>
+			<main className="home-chr" style={{ display: "none" }}>
 				<div className="home__top-chr">
 					<div className="home__top-inner-chr">
 						<img className="floated-img" src={img} alt="" />
 						<div className="home__top-date-chr">
 							{envelope.date} {txtMonth} {envelope.year} рік
 						</div>
-						<p className="home__top-title-chr">Таїнство хрещення Марійки</p>
-						{/* <p className="home__top-title-chr">Хрестини Марійки</p> */}
+						<p className="home__top-title-chr">Таїнство хрещення Марії</p>
+						{/* <p className="home__top-title-chr">Хрестини Марії</p> */}
 					</div>
 					<div className="rainbow">
 						<div className="pp"></div>
@@ -233,17 +287,33 @@ const ChristeningBronze = () => {
 					</div>
 				</div>
 				<div className="wrapper">
-					<img className="floated-img-3" src={img2} alt="" />
-					<p className="pepe-chr">
+					<div className="swiper-cont animated-element1">
+						<img
+							className="iimg"
+							src={envelope.gallery[3]}
+							alt=""
+							loading="lazy"
+						/>
+						<img className="gallery-float-img" src={img4} alt="" />
+					</div>
+					{/* <div className="gallery-masonry">
+							{envelope.gallery.map((img, index) => {
+								return <img key={index} src={img} alt="" loading="lazy" />;
+							})}
+						</div> */}
+
+					<img className="floated-img-3 animated-element1" src={img2} alt="" />
+					<p className="pepe-chr animated-element1">
 						<span>Дорогі гості,</span>
 						<br />з великою радістю та любов’ю запрошуємо Вас розділити з нами
 						важливу подію
 						<br />
-						Таїнство Хрещення нашої донечки Марійки
+						<br />
+						Таїнство Хрещення нашої донечки Марії
 					</p>
 				</div>
 
-				<div className="calendar-wrapper-chr">
+				<div className="calendar-wrapper-chr animated-element1">
 					<p className="calendar-top-christening">{`${txtMonth} ${envelope.year}`}</p>
 					<div className="calendar-christening">
 						<div>Пн</div>
@@ -270,44 +340,62 @@ const ChristeningBronze = () => {
 						})}
 					</div>
 				</div>
-				<p className="pepe-chr">
+				<p className="pepe-chr animated-element1">
 					І ми не уявляємо цей радісний день без Вас - близьких і дорогих нам
 					людей!
 				</p>
-				<div className="wrapper">
-					<img className="floated-img-3" src={img2} alt="" />
+				<div style={{ padding: 10 }} className="wrapper">
+					<img className="floated-img-3 animated-element1" src={img2} alt="" />
 					<div className="addresses-container">
-						<p className="addresses__title-chr">Адреси святкування</p>
-						<p style={{ marginBottom: 25 }} className="page-desc-chr">
-							Київський час
+						<p className="addresses__title-chr animated-element1">
+							Адреси святкування
+						</p>
+						<p
+							style={{ marginBottom: 25 }}
+							className="page-desc-chr animated-element1"
+						>
+							Празький час
 						</p>
 						<div className="addresses-chr">
 							{envelope.adresess.map((address, index) => {
 								return (
 									<div key={index} className="address-chr">
-										<p className="address__title-chr">
+										<p className="address__title-chr animated-element1">
 											<span>{address.title}</span>
 											<span>{address.time}</span>
 										</p>
-										<p className="address__info-chr">{address.address_title}</p>
+										<p className="address__info-chr animated-element1">
+											{address.address_title}
+										</p>
 										<p
 											style={{ marginBottom: 10 }}
-											className="address__info-chr"
+											className="address__info-chr animated-element1"
 										>
 											{address.address}
 										</p>
 										<iframe
-											className="map"
+											className="map animated-element1"
 											src={address.address_url}
 											loading="lazy"
 										></iframe>
 										<a
-											className="address__link-chr"
+											className="address__link-chr animated-element1"
 											href={address.address_destination_url}
 											target="_blank"
 										>
 											Отримати маршрут
 										</a>
+										{/* <img
+											style={{
+												position: "absolute",
+												top: 100,
+												right: 0,
+												borderRadius: 20,
+											}}
+											src={address.img}
+											width={200}
+											alt=""
+										/> */}
 									</div>
 								);
 							})}
@@ -315,22 +403,34 @@ const ChristeningBronze = () => {
 					</div>
 				</div>
 
-				<div className="wrapper">
-					<img className="floated-img-3" src={img2} alt="" />
-					<div className="gallery-chr">
-						<p className="page-title-chr">Галерея</p>
-						<div className="gallery-masonry">
+				<div style={{ padding: "10px" }} className="wrapper">
+					<div className="swiper-cont-2 animated-element1">
+						<img
+							className="iimg"
+							src={envelope.gallery[0]}
+							alt=""
+							loading="lazy"
+						/>
+						<img className="gallery-float-img-2" src={img5} alt="" />
+					</div>
+					{/* <img className="floated-img-3" src={img2} alt="" /> */}
+					{/* <div className="gallery-chr"> */}
+					{/* <p className="page-title-chr">Галерея</p> */}
+					{/* <div className="gallery-masonry">
 							{envelope.gallery.map((img, index) => {
 								return <img key={index} src={img} alt="" loading="lazy" />;
 							})}
-						</div>
-					</div>
+						</div> */}
+
+					{/* </div> */}
 				</div>
 				<div className="wrapper">
-					<img className="floated-img-3" src={img2} alt="" />
+					{/* <img className="floated-img-3" src={img2} alt="" /> */}
 					<div className="date-container-chr">
-						<p className="page-title-chr">Святкування почнеться через:</p>
-						<div className="date-chr" id="date">
+						<p className="page-title-chr animated-element1">
+							Святкування почнеться через:
+						</p>
+						<div className="date-chr animated-element1" id="date">
 							<div>
 								<p>{days}</p>
 								<p>днів</p>
@@ -351,14 +451,26 @@ const ChristeningBronze = () => {
 					</div>
 				</div>
 				<div className="wrapper">
-					<img className="floated-img-3" width={150} src={img3} alt="" />
-					<p className="page-title-chr">
+					<img className="floated-img-3 animated-element1" src={img2} alt="" />
+					<p className="page-title-chr animated-element1">
 						З любов’ю,
 						<br />
 						{envelope.mother_name} та {envelope.father_name}
 					</p>
+					<div style={{ padding: 10 }}>
+						<div className="swiper-cont-2 animated-element1">
+							<img
+								className="iimg"
+								src={envelope.gallery[1]}
+								alt=""
+								loading="lazy"
+							/>
+							<img className="gallery-float-img-2" src={img6} alt="" />
+						</div>
+					</div>
+
 					<div>
-						<div className="rainbow-bottom">
+						<div className="rainbow-bottom animated-element1">
 							<div className="rainbow">
 								<div className="pp"></div>
 								<div className="pp"></div>
@@ -371,10 +483,18 @@ const ChristeningBronze = () => {
 								<div className="pp"></div>
 							</div>
 						</div>
-						<img className="rainbow-img-3" src={img1} alt="" />
+						<img
+							className="rainbow-img-3 animated-element1"
+							src={img1}
+							alt=""
+						/>
 					</div>
 				</div>
 			</main>
+			<button onClick={handlePlayAudio} className="floating-btn-2">
+				<img src={isPlaying === false ? playIcon : pauseIcon} alt="" />
+			</button>
+			<audio className="audio" autoPlay src={envelope.song}></audio>
 		</>
 	);
 };
