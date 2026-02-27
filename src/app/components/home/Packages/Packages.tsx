7560a84c66b11b653d34eea4e5953df2a30388c8
Stepan Dordiai <stepandordiai@gmail.com>
2026-02-27 09:34:14 +0100

    feat: update Packages component

--- src/app/components/home/Packages/Packages.tsx
+++ src/app/components/home/Packages/Packages.tsx
@@ -129,7 +129,19 @@ const Packages = () => {
 													>
 														<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
 													</svg>{" "}
-													<span>{t(option.name)}</span>
+													<span
+														style={
+															option.isDiscounted
+																? { fontWeight: 500 }
+																: { fontWeight: 400 }
+														}
+													>
+														{t(option.name)}
+														<br />
+														{option.isDiscounted && (
+															<span>{t(option.discount)}</span>
+														)}
+													</span>
 												</div>
 											);
 										})}
