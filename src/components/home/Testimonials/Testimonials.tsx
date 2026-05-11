7bb76f35ee58d93b2e2f41d683a0a35360783710
Stepan Dordiai <stepandordiai@gmail.com>
2026-05-11 23:38:47 +0200

    feat: update Testimonials section

--- src/components/home/Testimonials/Testimonials.tsx
+++ src/components/home/Testimonials/Testimonials.tsx
@@ -9,68 +9,88 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Autoplay, Pagination } from "swiper/modules";
-
+import { testimonials } from "./testimonials";
 import styles from "./Testimonials.module.scss";
 
-const testimonials = [
-	{
-		client: "Емілія та Владислав",
-		label:
-			'"Дуже сподобалося наше запрошення на хрестини ніжне, стильне й атмосферне, саме так, як ми і хотіли. Дякуємо за увагу до деталей, врахування всіх побажань і швидку роботу. Було приємно співпрацювати, результат перевершив наші очікування! 😍😍🥰"',
-		img: "/testimonial-1.svg",
-	},
-	{
-		client: "Андрій та Наталія",
-		label:
-			'"Дуже вдячні вам за таке креативне весільне запрошення — усім воно дуже сподобалося! Це було надзвичайно практично, а також хочемо зазначити, що замовлення було виконано у неймовірно короткий термін. Щиро дякуємо! 🫂🎉 Моя рекомендація"',
-		img: "/testimonial-2.svg",
-	},
-	{
-		client: "Михайло та Мирослава",
-		label:
-			'"Щиро дякую за гарну роботу. Нам і нашим гостям  дуже сподобалося зроблене вами запрошення, на визначну для нас подію. Обов\'язково від нас буде рекомендація Вас, як професіонала своєї справи. Успіхів і усього найкращого. Приємно з вами працювати!!!"',
-		img: "/testimonial-3.svg",
-	},
-];
-
 const Testimonials = () => {
+	const reviewSchema = {
+		"@context": "https://schema.org",
+		"@type": "Organization",
+		name: "pixel flower",
+
+		review: testimonials.map((t) => ({
+			"@type": "Review",
+
+			author: {
+				"@type": "Person",
+				name: t.client,
+			},
+
+			reviewBody: t.label,
+
+			reviewRating: {
+				"@type": "Rating",
+				ratingValue: 5,
+				bestRating: 5,
+			},
+		})),
+	};
+
+	// TODO: ?
+	// 	export async function generateMetadata() {
+	//   return {
+	//     other: {
+	//       "script:ld+json": JSON.stringify(reviewSchema),
+	//     },
+	//   };
+	// }
+
 	return (
-		<section className={styles.section} id="testimonials">
-			<h2 className={styles["section__title"]}>Відгуки наших клієнтів</h2>
-			<Swiper
-				spaceBetween={100}
-				pagination={{
-					clickable: true,
-					renderBullet: (index, className) => {
-						return `<img src="${testimonials[index].img}" class="${className}" alt="testimonial ${index}" />`;
-					},
-				}}
-				autoplay={{
-					delay: 5000,
-					disableOnInteraction: false,
-				}}
-				speed={1000}
-				loop={true}
-				modules={[Pagination, Autoplay]}
-				className={styles["mySwiper"]}
-			>
-				{testimonials.map((t, i) => (
-					<SwiperSlide key={i}>
-						<div>
-							<p
-								style={{
-									paddingBottom: 25,
-									fontSize: "clamp(18px, 4vw, 24px)",
-								}}
-							>
-								{t.label}
-							</p>
-							<p style={{ fontWeight: 500 }}>{t.client}</p>
-						</div>
-					</SwiperSlide>
-				))}
-			</Swiper>
-		</section>
+		<>
+			<script
+				type="application/ld+json"
+				dangerouslySetInnerHTML={{ __html: JSON.stringify(reviewSchema) }}
+			/>
+			<section className={styles.section} id="testimonials">
+				<h2 className={styles["section__title"]}>Відгуки наших клієнтів</h2>
+				<Swiper
+					spaceBetween={100}
+					pagination={{
+						clickable: true,
+						renderBullet: (index, className) => {
+							return `<img src="${testimonials[index].img}" class="${className}" alt="testimonial ${index}" />`;
+						},
+					}}
+					autoplay={{
+						delay: 5000,
+						disableOnInteraction: false,
+					}}
+					speed={1000}
+					loop={true}
+					modules={[Pagination, Autoplay]}
+					className={styles["mySwiper"]}
+				>
+					{testimonials.map((t, i) => (
+						<SwiperSlide key={i}>
+							{/* TODO: learn this */}
+							<figure>
+								<blockquote
+									style={{
+										paddingBottom: 25,
+										fontSize: "clamp(18px, 4vw, 24px)",
+									}}
+								>
+									<p>{t.label}</p>
+								</blockquote>
+								<figcaption style={{ fontWeight: 500 }}>
+									<cite>{t.client}</cite>
+								</figcaption>
+							</figure>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</section>
+		</>
 	);
 };
 
